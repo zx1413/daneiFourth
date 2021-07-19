ba6b5ef164c4ff9f932161fc7a4f5f37958cd3ae
zx1413 <1346782573@qq.com>
2021-07-19 19:24:56 +0800

    addnew

--- myspring-thsecurity/src/main/java/cn/dszx/myapplication.java
+++ myspring-thsecurity/src/main/java/cn/dszx/myapplication.java
@@ -2,10 +2,25 @@ package cn.dszx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.DigestUtils;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class myapplication {
     public static void main(String[] args) {
         SpringApplication.run(myapplication.class,args);
+        test();
+    }
+    public static void test(){
+        for (int i = 0; i < 20; i++) {
+            String pass = new BCryptPasswordEncoder().encode("123456");
+//            String s = DigestUtils.md5DigestAsHex("123456".getBytes());
+//            System.out.println(s);
+            System.out.println(pass);
+        }
+
     }
 }
