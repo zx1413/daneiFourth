18fcd6280c6b9d148f546ddf6bdea7e975e67a8b
zx1413 <1346782573@qq.com>
2021-07-19 16:13:04 +0800

    addnew

---
+++ myspring-thsecurity/src/main/java/cn/dszx/myapplication.java
@@ -0,0 +1,11 @@
+package cn.dszx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(myapplication.class,args);
+    }
+}
